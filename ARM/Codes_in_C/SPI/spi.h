52439ab408bee9e1f8030ae831668853713d4ae3
sanket7941 <sanketn7941@gmail.com>
2018-09-25 22:07:03 +0530

    spi need to work

---
+++ ARM/Codes_in_C/SPI/spi.h
@@ -0,0 +1,10 @@
+#ifndef __SPI_H
+#define __SPI_H
+
+#include "LPC17xx.h"
+
+#define SPCR_DL   2
+#define SPCR_CPHA 3
+#define SPCR_CPOL 4
+#define SPCR_LSBF 6
+ 
---
+++ ARM/Codes_in_C/TIME/TIME_RTC/buzz.h
@@ -0,0 +1,18 @@
+#ifndef __BUZZ_H
+#define __BUZZ_H
+
+#include"LPC17xx.h"
+
+#define port2 LPC_GPIO2
+
+#define buzzer 11
+
+void BuzzInit(void);
+
+void BuzzOn(void);
+
+void BuzzOff(void);
+
+void BuzzNote1(int s);
+
+#endif
